a9156d1d37c50afe8dd5187f28b36e992dc98eb0
Jeffrey C. Ollie <jeff@ocjtech.us>
2024-12-30 18:11:49 -0600

    use system fontconfig and glslang

--- nix/package.nix
+++ nix/package.nix
@@ -19,6 +19,7 @@
   wrapGAppsHook4,
   gsettings-desktop-schemas,
   git,
+  glslang,
   ncurses,
   pkg-config,
   zig_0_13,
@@ -121,13 +122,17 @@ in
 
     dontConfigure = true;
 
-    zigBuildFlags = [
-      "--system"
-      "${finalAttrs.deps}"
-      "-Dversion-string=${finalAttrs.version}-${revision}-nix"
-      "-Dgtk-x11=${lib.boolToString enableX11}"
-      "-Dgtk-wayland=${lib.boolToString enableWayland}"
-    ];
+    zigBuildFlags =
+      [
+        "--system"
+        "${finalAttrs.deps}"
+        "-Dversion-string=${finalAttrs.version}-${revision}-nix"
+        "-Dgtk-x11=${lib.boolToString enableX11}"
+        "-Dgtk-wayland=${lib.boolToString enableWayland}"
+      ]
+      ++ lib.mapAttrsToList (name: package: "-fsys=${name} --search-prefix ${lib.getLib package}") {
+        inherit glslang;
+      };
 
     outputs = [
       "out"
