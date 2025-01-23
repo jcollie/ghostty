46801e9c9a5ac0a0c9d02920cb041347d2dd269c
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-01-23 13:57:36 -0600

    gtk: implement audio bell

--- nix/devShell.nix
+++ nix/devShell.nix
@@ -33,6 +33,7 @@
   gtk4,
   gtk4-layer-shell,
   gobject-introspection,
+  gst_all_1,
   libadwaita,
   blueprint-compiler,
   gettext,
@@ -158,6 +159,9 @@ in
         wayland
         wayland-scanner
         wayland-protocols
+        gst_all_1.gstreamer
+        gst_all_1.gst-plugins-base
+        gst_all_1.gst-plugins-good
       ];
 
     # This should be set onto the rpath of the ghostty binary if you want
