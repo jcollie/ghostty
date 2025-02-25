e3982238a665b3f746f84385ebbc6fef1acccacd
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-02-25 11:59:46 -0600

    core/gtk/glfw: open urls using an apprt action instead of doing it directly
    Partial implementation of #5256
    
    This implements the core changes necessary to open urls using an apprt
    action rather than doing it directly from the core.
    
    Implements the open_url action in the GTK and GLFW apprts.
    
    Note that this should not be merged until a macOS-savvy developer can add
    an implementation of the open_url action for the macOS apprt.

--- src/Surface.zig
+++ src/Surface.zig
@@ -3312,7 +3312,11 @@ fn processLinks(self: *Surface, pos: apprt.CursorPos) !bool {
                 .trim = false,
             });
             defer self.alloc.free(str);
-            try internal_os.open(self.alloc, .unknown, str);
+            _ = try self.rt_app.performAction(
+                .{ .surface = self },
+                .open_url,
+                .{ .kind = .unknown, .url = str },
+            );
         },
 
         ._open_osc8 => {
@@ -3320,7 +3324,11 @@ fn processLinks(self: *Surface, pos: apprt.CursorPos) !bool {
                 log.warn("failed to get URI for OSC8 hyperlink", .{});
                 return false;
             };
-            try internal_os.open(self.alloc, .unknown, uri);
+            _ = try self.rt_app.performAction(
+                .{ .surface = self },
+                .open_url,
+                .{ .kind = .unknown, .url = uri },
+            );
         },
     }
 
@@ -4476,7 +4484,13 @@ fn writeScreenFile(
     const path = try tmp_dir.dir.realpath(filename, &path_buf);
 
     switch (write_action) {
-        .open => try internal_os.open(self.alloc, .text, path),
+        .open => {
+            _ = try self.rt_app.performAction(
+                .{ .surface = self },
+                .open_url,
+                .{ .kind = .text, .url = path },
+            );
+        },
         .paste => self.io.queueMessage(try termio.Message.writeReq(
             self.alloc,
             path,
