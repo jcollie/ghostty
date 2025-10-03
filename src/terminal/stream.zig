c4de98e7013a8ccfcacef994cfbdf64cde92912d
Jeffrey C. Ollie <jeff@ocjtech.us>
2025-10-03 13:54:38 -0500

    osc rewrite wip
    osc rewrite wip
    
    osc rewrite wip
    
    add urldecode function
    
    osc rewrite wip
    
    osc rewrite wip
    
    osc rewrite wip
    
    osc wip
    
    osc rewrite wip
    
    osc rewrite wip
    
    osc rewrite wip
    
    osc rewrite wip
    
    osc rewrite wip
    
    osc rewrite wip

--- src/terminal/stream.zig
+++ src/terminal/stream.zig
@@ -2123,6 +2123,7 @@ pub fn Stream(comptime Handler: type) type {
                 .conemu_change_tab_title,
                 .conemu_wait_input,
                 .conemu_guimacro,
+                .conemu_comment,
                 => {
                     log.warn("unimplemented OSC callback: {}", .{cmd});
                 },
