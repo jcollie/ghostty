8c2bf14b031869c05d5f5a2b51ed6106945b7770
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-01-27 10:30:23 -0600

    osc: parse the Kitty desktop notification protocol (OSC 99)
    This includes only parsing of the OSC. You cannot use OSC 99 to send
    notifications. Uses lazy parsing of the metadata modelled on the new OSC
    133 behavior.

--- src/terminal/stream.zig
+++ src/terminal/stream.zig
@@ -2045,6 +2045,7 @@ pub fn Stream(comptime Handler: type) type {
                 .conemu_output_environment_variable,
                 .conemu_run_process,
                 .kitty_text_sizing,
+                .kitty_desktop_notification,
                 => {
                     log.debug("unimplemented OSC callback: {}", .{cmd});
                 },
