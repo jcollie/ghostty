4056e39a41f6d60422be19a61fe941c9a018998e
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-02-13 08:44:36 -0600

    snapshot wip

--- include/ghostty.h
+++ include/ghostty.h
@@ -630,6 +630,13 @@ typedef enum {
   GHOSTTY_READONLY_ON,
 } ghostty_action_readonly_e;
 
+// apprt.action.Snapshot
+typedef enum {
+  GHOSTTY_SNAPSHOT_COPY,
+  GHOSTTY_SNAPSHOT_PASTE,
+  GHOSTTY_SNAPSHOT_OPEN,
+} ghostty_action_snapshot_e;
+
 // apprt.action.DesktopNotification.C
 typedef struct {
   const char* title;
@@ -927,7 +934,11 @@ typedef enum {
   GHOSTTY_ACTION_SEARCH_TOTAL,
   GHOSTTY_ACTION_SEARCH_SELECTED,
   GHOSTTY_ACTION_READONLY,
+<<<<<<< HEAD
   GHOSTTY_ACTION_COPY_TITLE_TO_CLIPBOARD,
+=======
+  GHOSTTY_ACTION_TAKE_SNAPSHOT,
+>>>>>>> 427b34e63 (snapshot wip)
 } ghostty_action_tag_e;
 
 typedef union {
@@ -968,6 +979,7 @@ typedef union {
   ghostty_action_search_total_s search_total;
   ghostty_action_search_selected_s search_selected;
   ghostty_action_readonly_e readonly;
+  ghostty_action_snapshot_e snapshot;
 } ghostty_action_u;
 
 typedef struct {
