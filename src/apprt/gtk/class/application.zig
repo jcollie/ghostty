e254b9591445da217479efe225f9864ad174855c
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-03-17 17:13:11 -0500

    core: take_screenshot → capture_screenshot

--- src/apprt/gtk/class/application.zig
+++ src/apprt/gtk/class/application.zig
@@ -784,7 +784,7 @@ pub const Application = extern struct {
             .check_for_updates,
             .undo,
             .redo,
-            .take_screenshot,
+            .capture_screenshot,
             => {
                 log.warn("unimplemented action={}", .{action});
                 return false;
