5bba0fe4baa9a40cc41b7b2954a2561bed0f745f
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-03-17 13:05:30 -0500

    core: add take_screenshot keybind and apprt action
    Advantages over OS native screenshot facilities:
    
    * Easier to capture just a single surface without capturing parts of
      other splits.
    * Triggerable using configurable Ghostty keybind.
    * Triggerable from APIs like Applescript or other future API interfaces
      (CI perhaps?).
    * Doesn't include any decoration added by window manager or other GUI
      elements.
    * Doesn't include background if the surface is transparent.
    * Ability to screenshot non-visible surfaces (using an API, keybind
      would only work on active surface).
    
    Disadvantages over OS native screenshot facilities:
    
    * Only captures a single surface, so can't include multiple splits.
    * Doesn't include window manager decorations, or background if the
      surface is partially transparent.
    
    The disadvantages aren't "real" disadvantages as including the
    `take_screenshot` action doesn't preclude the use of OS-native
    screenshot facilities (although scripting those may be more difficult
    depending on the platform).

--- src/apprt/gtk/class/application.zig
+++ src/apprt/gtk/class/application.zig
@@ -787,6 +787,7 @@ pub const Application = extern struct {
             .check_for_updates,
             .undo,
             .redo,
+            .take_screenshot,
             => {
                 log.warn("unimplemented action={}", .{action});
                 return false;
