d9255af095f4b4c960962416b581e37e420f55b5
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-08-07 11:36:16 -0500

    use zst compressed nixpkgs

--- flake.nix
+++ flake.nix
@@ -6,10 +6,10 @@
     # glibc versions used by our dependencies from Nix are compatible with the
     # system glibc that the user is building for.
     #
-    # We are currently on nixpkgs-unstable to get Zig 0.15 for our package.nix and
-    # Gnome 49/Gtk 4.20.
+    # We are currently on nixpkgs-unstable to get Zig 0.16 for our package.nix,
+    # Gnome 50/Gtk 4.22, and fontconfig 2.18.
     #
-    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
 
     # Used for shell.nix
     flake-compat = {
