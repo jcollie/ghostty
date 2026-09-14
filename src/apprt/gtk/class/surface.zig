02f7b0b53cfe42756ba4d929088a90f448ce48c6
Jeffrey C. Ollie <jeff@ocjtech.us>
2026-09-14 15:09:03 -0500

    gtk: fix stray gl_area left over

--- src/apprt/gtk/class/surface.zig
+++ src/apprt/gtk/class/surface.zig
@@ -4720,7 +4720,7 @@ const Clipboard = struct {
             // attached.
             .kitty_read => |kitty| {
                 const clipboard = get(
-                    self.private().gl_area.as(gtk.Widget),
+                    self.private().render_surface.as(gtk.Widget),
                     kitty.location,
                 ) orelse {
                     surface.denyClipboardRequest(req.*);
